a248573f9354d9206dda53f6bc20235b9d13acf3
Manuel Aguilar <manguilar22@gmail.com>
2019-12-17 22:00:42 -0700

    first

---
+++ src/test/java/guru/aguilar/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.aguilar.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
