5d353822c7778932d1fbd363ac894723b6a54982
Manuel Aguilar <manguilar22@gmail.com>
2019-12-31 10:08:48 -0700

    update@Tue Dec 31 10:08:48 MST 2019

--- src/main/java/guru/aguilar/demo/controller/PetController.java
+++ src/main/java/guru/aguilar/demo/controller/PetController.java
@@ -1,9 +1,14 @@
 package guru.aguilar.demo.controller;
 
+import com.google.gson.Gson;
 import guru.aguilar.demo.domain.Pet;
 import guru.aguilar.demo.repo.PetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Optional;
 
 /**
  * curl http://localhost:8080/pet/add -H "Content-type:application/json" -d ' { "name" : "kjdsjds","type":"fdsjfdsjk"} ' -X POST
@@ -27,6 +32,8 @@ public class PetController {
 
     @PostMapping("/add")
     public @ResponseBody String savePetToDatabase(@RequestBody Pet pet){
+        Gson gson = new Gson();
+        System.out.println("-> POST:\t"+gson.toJson(pet));
         petRepository.save(pet);
         return "SAVED";
     }
@@ -34,6 +41,12 @@ public class PetController {
     @GetMapping("/db")
     public @ResponseBody Iterable<Pet> getAllPets(){
         return petRepository.findAll();
+    }
+
+    @GetMapping("/db/{id}")
+    public @ResponseBody Pet getOnePetBasedOnId(@PathVariable Long id){
+        Optional<Pet> op = petRepository.findById(id);
+        return op.isPresent() ? op.get() : null;
     }
 
 
