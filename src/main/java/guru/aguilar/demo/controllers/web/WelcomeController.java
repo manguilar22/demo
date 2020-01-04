b20d235ba71716dd958bc43cd9eea656a6396f05
Manuel Aguilar <manguilar22@gmail.com>
2020-01-04 12:11:06 -0700

    fix@Sat Jan  4 12:11:06 MST 2020

--- src/main/java/guru/aguilar/demo/controllers/web/WelcomeController.java
+++ src/main/java/guru/aguilar/demo/controllers/web/WelcomeController.java
@@ -4,31 +4,47 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 @Controller
 public class WelcomeController {
 
 
+
+
+
     @GetMapping("/hello")
     public String main(Model model) {
         model.addAttribute("message", "Simple MESSAGE");
-        model.addAttribute("tasks", "SImple TASK");
+        model.addAttribute("title", "SImple TASK");
 
         return "welcome"; //view
     }
 
     // /say?name=kotlin
     @GetMapping("/say")
-    public String mainWithParam(
-            @RequestParam(name = "name", required = false, defaultValue = "")
-                    String name, Model model) {
-
-        model.addAttribute("message", name);
-
+    public String mainWithParam(Model model ,@RequestParam(name = "name", required = false, defaultValue = "stranger") String name) {
+        model.addAttribute("messages",name);
         return "welcome"; //view
+    }
+
+
+    @GetMapping("/saying")
+    public String logMessagesWithParam(Model model){
+
+        List<String> data = Arrays.asList(new String[]{"lfsaddl","lkdaf","lkds"});
+        model.addAttribute("logs",data);
+        return "welcome";
+    }
+
+    //@ModelAttribute("logs")
+    public List<String> logMessages(){
+        List<String> data = Arrays.asList(new String[]{"lfsaddl","lkdaf","lkds"});
+        return data;
     }
 
 }
